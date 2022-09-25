ec64bb565dee2f9a29de2f74344d9255b779db58
Max Levy <maxim.levy@rakuten.com>
2022-09-25 08:23:31 +0000

    switch linter to pylama

--- try_x.py
+++ try_x.py
@@ -1,16 +1,14 @@
 """To see check if X-system is operational"""
 import cv2
-import numpy as np
 
-
+print(cv2.__version__)
 
 IMG_NAME = './data/set_5296.jpg'
 
-img = cv2.imread(IMG_NAME,1)
-cv2.imshow(IMG_NAME,img)
+img = cv2.imread(IMG_NAME, 1)
+cv2.imshow(IMG_NAME, img)
 
-
+cv2.imreadmulti()
 
 print("Press any key to quit")
 cv2.waitKey(0)
-
